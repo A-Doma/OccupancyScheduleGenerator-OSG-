bfcc3786fcae2fbaf8ba29081febc29299c17124
Aya Doma <92944753+AyaDoma@users.noreply.github.com>
2024-02-12 17:46:10 -0500

    Update __init__.py

--- OSG/__init__.py
+++ OSG/__init__.py
@@ -5,9 +5,6 @@ Your package's docstring with a description of its purpose, usage, etc.
 # Import the main entry point function or class from main.py
 import OSG
 
-# Import relevant functions/classes from aggregator_functions.py that should be available to the user
-import aggregator_functions 
-
 # Define your package's version
 __version__ = '0.1.0'
 
