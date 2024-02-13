a5e123f31e4fff4d4ae9874329639601d845fa53
Aya Doma <92944753+AyaDoma@users.noreply.github.com>
2024-02-13 15:56:26 -0500

    Update OSG.py

--- OSG/OSG.py
+++ OSG/OSG.py
@@ -241,7 +241,7 @@ def occupancy_status_profile(df_houses: pd.DataFrame(), wd):
     df_final['date_time'] = df_final['date'] + pd.to_timedelta(df_final['hour'], unit='h')
     df_final = df_final.drop(['date', 'hour'], axis=1)
     df_final= df_final.sort_values(by=['Identifier','date_time'])
-    df_final_leg= df_final[df_final['number_sensors']>2]
+    df_final_leg= df_final[df_final['number_sensors']>=2]
     columns_order = ['date_time', 'Identifier'] + [col for col in df_final_leg.columns if col not in ['date_time', 'Identifier']]
     data_reordered = df_final_leg[columns_order]
     data_reordered.reset_index(inplace=True)
